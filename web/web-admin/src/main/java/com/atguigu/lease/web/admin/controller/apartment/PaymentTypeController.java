a6ff7e4fd2af906adb78b41eebacd447bca7962c
Xu <2265035472@qq.com>
2026-01-01 22:29:12 +0800

    支付方式管理

--- web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/apartment/PaymentTypeController.java
+++ web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/apartment/PaymentTypeController.java
@@ -3,8 +3,10 @@ package com.atguigu.lease.web.admin.controller.apartment;
 
 import com.atguigu.lease.common.result.Result;
 import com.atguigu.lease.model.entity.PaymentType;
+import com.atguigu.lease.web.admin.service.PaymentTypeService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,23 +15,29 @@ import java.util.List;
 @Tag(name = "支付方式管理")
 @RequestMapping("/admin/payment")
 @RestController
+@RequiredArgsConstructor
 public class PaymentTypeController {
 
+    private final PaymentTypeService paymentTypeService;
+
     @Operation(summary = "查询全部支付方式列表")
-    @GetMapping("list")
+    @GetMapping("/list")
     public Result<List<PaymentType>> listPaymentType() {
-        return Result.ok();
+        List<PaymentType> list = paymentTypeService.list();
+        return Result.ok(list);
     }
 
     @Operation(summary = "保存或更新支付方式")
-    @PostMapping("saveOrUpdate")
+    @PostMapping("/saveOrUpdate")
     public Result saveOrUpdatePaymentType(@RequestBody PaymentType paymentType) {
+        paymentTypeService.save(paymentType);
         return Result.ok();
     }
 
     @Operation(summary = "根据ID删除支付方式")
-    @DeleteMapping("deleteById")
+    @DeleteMapping("/deleteById")
     public Result deletePaymentById(@RequestParam Long id) {
+        paymentTypeService.removeById(id);
         return Result.ok();
     }
 
