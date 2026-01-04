1bffb3cd2f8d182a99cc83dc37f35a9f97496291
Xu <2265035472@qq.com>
2026-01-04 16:13:39 +0800

    Mybatisplus的分页配置

---
+++ common/src/main/java/com/atguigu/lease/common/mybatisplus/MybatisPlusConfiguration.java
@@ -0,0 +1,4 @@
+package com.atguigu.lease.common.mybatisplus;
+
+public class MybatisPlusConfiguration {
+}
--- web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/apartment/ApartmentController.java
+++ web/web-admin/src/main/java/com/atguigu/lease/web/admin/controller/apartment/ApartmentController.java
@@ -34,8 +34,9 @@ public class ApartmentController {
     }
 
     @Operation(summary = "根据条件分页查询公寓列表")
-    @GetMapping("pageItem")
+    @GetMapping("/pageItem")
     public Result<IPage<ApartmentItemVo>> pageItem(@RequestParam long current, @RequestParam long size, ApartmentQueryVo queryVo) {
+
         return Result.ok();
     }
 
